25d0a4afce7b1143e9bc53e37d8d681216d1ca38
nevter74 <nevter74@outlook.com>
2025-06-24 15:47:15 +0200

    v1

---
+++ src/MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass {
+
+    //Version v1
+    public String string1 = "Bu bir string'dir.";
+
+    //Version v2
+    public String string2 = "Bu da baska bir string'dir.";
+
+    //Version v3
+    public String string3 = "Bu da cok daha baska bir string'dir.";
+}
