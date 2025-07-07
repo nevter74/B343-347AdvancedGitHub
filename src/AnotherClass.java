0374fe301e537fef0c109f9c565410a25b846a25
nevter74 <nevter74@outlook.com>
2025-07-07 21:51:35 +0200

    t8

--- src/AnotherClass.java
+++ src/AnotherClass.java
@@ -4,4 +4,6 @@ public class AnotherClass {
 
     //Techpro branch, version t7
     public String string2 = "merhaba dunya";
+    //Techpro branch, t8
+    public String string3 = "Parildayip duran insanlara bak";
 }
