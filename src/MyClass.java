209eeab9320af3ef9df671320f0931c26efd5416
nevter74 <nevter74@outlook.com>
2025-07-07 20:50:13 +0200

    v7

--- src/MyClass.java
+++ src/MyClass.java
@@ -26,7 +26,11 @@ public class MyClass {
     public String string8 = "Dar geldi ankara.";
 
     //Techpro branch, version t5
-    public String string10 = "Affet beni aksam ustu";
+    public String string9 = "Affet beni aksam ustu";
     //Conflict example, t6
-    public String string11 = "Bu aksam olurum kimse beni tutamaz";
+    public String string10 = "Bu aksam  kimse beni ";
+    //Conflict example, v7
+    public String string11 = "Eski koprunun altinda";
+
+
 }
